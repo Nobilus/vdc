a304e47141441ddda90f046ef72570a0e9123325
Jonas De Meyer <jonas.demeyer@profunctional.be>
2024-05-25 12:19:25 +0200

    delcare env schema

---
+++ src/app/env.ts
@@ -0,0 +1,14 @@
+import { createEnv } from '@t3-oss/env-nextjs'
+import { z } from 'zod'
+
+export const env = createEnv({
+  server: {},
+  client: {
+    NEXT_PUBLIC_FB_PAGE_ID: z.number().min(1),
+    NEXT_PUBLIC_MAP_API_KEY: z.string().min(1),
+  },
+  runtimeEnv: {
+    NEXT_PUBLIC_FB_PAGE_ID: process.env.NEXT_PUBLIC_FB_PAGE_ID,
+    NEXT_PUBLIC_MAP_API_KEY: process.env.NEXT_PUBLIC_MAP_API_KEY,
+  },
+})
