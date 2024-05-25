bd0098c0658b0663299756c3bcf780f1887af45d
Jonas De Meyer <jonas.demeyer@profunctional.be>
2024-05-25 17:00:40 +0200

    remove all unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
 import { FBPost } from '@/components/fb/post'
 import { OpeningHours } from '@/components/openingHours'
-import { Data } from '@/types/google'
-import { convertToLuxonDateTime } from '@/utils'
-import { placesFields } from '@/utils/google'
-import axios from 'axios'
-import Image from 'next/image'
-import { env } from 'process'
 
 export default async function Home() {
   const postIds = ['pfbid02sj3bifJhY81tQcy1tRiC7K2cQCtdL93JoBpr1pmxaXKcFVhY2yv3FWG6TDLm3144l']
